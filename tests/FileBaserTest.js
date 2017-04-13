6c12fca4b6c03636ff39e8fa8ea2ba944d5f5647
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-04-12 23:40:01 -0300

    pretier tests/FireBaserTest.js

--- tests/FileBaserTest.js
+++ tests/FileBaserTest.js
@@ -1,49 +1,48 @@
-const assert = require('chai').assert;
+const assert = require("chai").assert;
 
-const FileBaser = require('./../src/FileBaser'),
-    Collection = require('./../src/Collection');
+const FileBaser = require("./../src/FileBaser"),
+  Collection = require("./../src/Collection");
 
-describe('DocFileTest', () => {
+describe("DocFileTest", () => {
+  describe("testAddingCollection", () => {
+    let db = new FileBaser("file-baser.json");
+    let collection = db.addCollection("datalist");
 
-    describe('testAddingCollection', () => {
-        let db = new FileBaser('file-baser.json');
-        let collection = db.addCollection('datalist');
-
-        it ('Should confirm collection is instance of Collection', () => {
-            assert.instanceOf(collection, Collection);
-        });
-
-        it('Should confirm collection\'s name', () => {
-            assert.equal('datalist', collection.getName());
-        });
+    it("Should confirm collection is instance of Collection", () => {
+      assert.instanceOf(collection, Collection);
     });
 
-    describe('testGettingCollection', () => {
-        let db = new FileBaser('file-baser.json'),
-            collection = db.addCollection('datalist');
+    it("Should confirm collection's name", () => {
+      assert.equal("datalist", collection.getName());
+    });
+  });
 
-        it ('Should confirm collection is instance of Collection', () => {
-            assert.instanceOf(collection, Collection);
-        });
+  describe("testGettingCollection", () => {
+    let db = new FileBaser("file-baser.json"),
+      collection = db.addCollection("datalist");
 
-        it('Should confirm collection\'s name', () => {
-            assert.equal('datalist', collection.getName());
-        });
-
-        it('Should throws Exception on unknown collection request', () => {
-            assert.throws(() => {
-                return db.getCollection('testing');
-            }, Error);
-        });
+    it("Should confirm collection is instance of Collection", () => {
+      assert.instanceOf(collection, Collection);
     });
 
-    describe('testDroppingDatabase', () => {
-
-        it('Should drop the database', () => {
-            let db = new FileBaser('file-baser.json');
-            assert.isTrue(db.dropDatabase());
-        });
-
+    it("Should confirm collection's name", () => {
+      assert.equal("datalist", collection.getName());
     });
 
+    it("Should throws Exception on unknown collection request", () => {
+      assert.throws(
+        () => {
+          return db.getCollection("testing");
+        },
+        Error
+      );
+    });
+  });
+
+  describe("testDroppingDatabase", () => {
+    it("Should drop the database", () => {
+      let db = new FileBaser("file-baser.json");
+      assert.isTrue(db.dropDatabase());
+    });
+  });
 });
