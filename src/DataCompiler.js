a0a84d043e1cb4e88c9a431d29d294f762f404ab
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-04-12 23:38:48 -0300

    prettier DataCompiler.js

--- src/DataCompiler.js
+++ src/DataCompiler.js
@@ -1,88 +1,75 @@
-const fs = require('fs');
+const fs = require("fs");
 
+class DataCompiler {
+  static checkDatabaseExists(database) {
+    return fs.existsSync(database);
+  }
 
-class DataCompiler
-{
+  static createDatabase(database) {
+    return fs.writeFileSync(
+      database,
+      JSON.stringify({
+        database: database,
+        collections: {}
+      })
+    );
+  }
 
-    static checkDatabaseExists(database)
-    {
-        return fs.existsSync(database);
+  static getDatabase(database) {
+    return JSON.parse(fs.readFileSync(database));
+  }
+
+  static dropDatabase(database) {
+    fs.unlinkSync(database);
+
+    return true;
+  }
+
+  static checkCollectionExists(database, collection) {
+    let data = this.getDatabase(database);
+
+    return typeof data.collections[collection] !== "undefined";
+  }
+
+  static createCollection(database, collection) {
+    let data = this.getDatabase(database);
+    data.collections[collection] = {
+      name: collection,
+      data: []
     };
 
-    static createDatabase(database)
-    {
-        return fs.writeFileSync(database, JSON.stringify({
-            database: database,
-            collections: {}
-        }));
-    };
+    return fs.writeFileSync(database, JSON.stringify(data));
+  }
 
-    static getDatabase(database)
-    {
-        return JSON.parse(fs.readFileSync(database));
-    };
+  static saveCollectionData(database, collection, collectionData, overWrite) {
+    let data = this.getDatabase(database);
 
-    static dropDatabase(database)
-    {
-        fs.unlinkSync(database);
+    if (overWrite || true) data.collections[collection].data = collectionData;
+    else data.collections[collection].data = data.collections[collection].data
+        .concat(collectionData)
+        .filter((element, index, allElements) => {
+          return allElements.indexOf(element) < 0;
+        });
 
-        return true;
-    };
+    fs.writeFileSync(database, JSON.stringify(data));
 
-    static checkCollectionExists(database, collection)
-    {
-        let data = this.getDatabase(database);
+    return true;
+  }
 
-        return typeof data.collections[collection] !== 'undefined';
-    };
+  static dropCollection(database, collection) {
+    let data = this.getDatabase(database);
+    delete data.collections[collection];
 
-    static createCollection(database, collection)
-    {
-        let data = this.getDatabase(database);
-        data.collections[collection] = {
-            name: collection,
-            data: []
-        };
+    fs.writeFileSync(database, JSON.stringify(data));
 
-        return fs.writeFileSync(database, JSON.stringify(data));
-    };
+    return true;
+  }
 
-    static saveCollectionData(database, collection, collectionData, overWrite)
-    {
-        let data = this.getDatabase(database);
+  static getCollection(database, collection) {
+    let data = this.getDatabase(database);
 
-        if (overWrite || true)
-            data.collections[collection].data = collectionData;
-        else
-            data.collections[collection].data = data.collections[collection]
-                .data
-                .concat(collectionData)
-                .filter((element, index, allElements) => {
-                    return allElements.indexOf(element) < 0;
-                });
-
-        fs.writeFileSync(database, JSON.stringify(data));
-
-        return true;
-    };
-
-    static dropCollection(database, collection)
-    {
-        let data = this.getDatabase(database);
-        delete data.collections[collection];
-
-        fs.writeFileSync(database, JSON.stringify(data));
-
-        return true;
-    };
-
-    static getCollection(database, collection)
-    {
-        let data = this.getDatabase(database);
-
-        return data.collections[collection];
-    };
-
-};
+    return data.collections[collection];
+  }
+}
 
 module.exports = DataCompiler;
