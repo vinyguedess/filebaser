63b200523e505796bded480b7ea165a2fc550b1a
Vinicius Guedes <viniciusgued@gmail.com>
2017-04-19 14:57:04 -0300

    Writing better tests for AsyncCollections

--- test/Unit/AsyncCollectionTest.js
+++ test/Unit/AsyncCollectionTest.js
@@ -4,12 +4,23 @@ const FileBaser = require("./../../src/FileBaser"),
   Collection = require("./../../src/Collection/Collection");
 
 describe("AsyncCollectionTest", () => {
-  describe("Doing something async way", () => {
-    let db = new FileBaser("file-baser.json");
+  let db = new FileBaser("file-baser.json");
 
+  before(() => {
+    let collection = db.addCollection("datalist");
+    collection.insert({ name: "Google Chrome", rate: 4.8 });
+    collection.insert({ name: "Mozilla Firefox", rate: 4.9 });
+    collection.insert({ name: "Opera", rate: 3.9 });
+    collection.insert({ name: "Internet Explorer", rate: 2.1 });
+    collection.flush();
+  });
+
+  describe("Doing something async way", () => {
     it("Should find data asynchronously", done => {
       db.getCollectionAsync("datalist").then(collection => {
         assert.instanceOf(collection, Collection);
+
+        assert.equal(4, collection.find().count());
 
         done();
       });
